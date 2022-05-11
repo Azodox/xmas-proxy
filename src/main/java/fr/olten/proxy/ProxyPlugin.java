d2a840014b3270cf3d655579237c0d08113799c5
Azodox_ <likemoi99@gmail.com>
2022-05-11 16:12:03 +0200

    Create config and register outgoing channel to communicate with lobby.

--- src/main/java/fr/olten/proxy/ProxyPlugin.java
+++ src/main/java/fr/olten/proxy/ProxyPlugin.java
@@ -6,15 +6,26 @@ import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.messages.ChannelIdentifier;
+import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
 import fr.olten.proxy.listener.PingListener;
+import fr.olten.proxy.listener.ServerConnectedListener;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
+import org.tomlj.Toml;
+import org.tomlj.TomlParseResult;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -24,9 +35,11 @@ public class ProxyPlugin {
     private final ProxyServer server;
     private final Logger logger;
     private final Path dataDirectory;
+    private TomlParseResult config;
+    private ChannelIdentifier bungeecordChannel;
 
     @Inject
-    public ProxyPlugin(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
+    public ProxyPlugin(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) throws IOException {
         this.server = server;
         this.logger = logger;
 
@@ -35,13 +48,48 @@ public class ProxyPlugin {
         }
 
         this.dataDirectory = dataDirectory;
+        this.initConfig();
 
         logger.info("Hi there!");
     }
 
+    private void initConfig() throws IOException {
+        var baseConfig = new File("config.toml");
+        var config = new File(dataDirectory.toFile(), "config.toml");
+
+        if(!config.exists()) {
+            config.createNewFile();
+        }
+
+        if(!FileUtils.contentEquals(config, baseConfig) && config.length() != 0){
+            logger.info("File '" + config.getPath() + "' content doesn't equals to '" + baseConfig.getPath() + "' and is not empty, skipping files copy.");
+        }else {
+            InputStream in = getClass().getResourceAsStream("/" + config.getName());
+            FileOutputStream out = new FileOutputStream(config);
+
+            byte[] buf = new byte[1024];
+            int n;
+
+            while ((n = in.read(buf)) >= 0) {
+                out.write(buf, 0, n);
+            }
+
+            in.close();
+            out.close();
+            logger.info("File '" + config.getPath() + "' copied from resources.");
+        }
+
+        this.config = Toml.parse(Paths.get(this.getDataDirectory() + File.separator + config.getName()));
+        this.config.errors().forEach(error -> System.err.println(error.toString()));
+    }
+
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event){
+        this.bungeecordChannel = MinecraftChannelIdentifier.create("xmas", "lobbysurvie");
+        server.getChannelRegistrar().register(bungeecordChannel);
+
         server.getEventManager().register(this, new PingListener(this));
+        server.getEventManager().register(this, new ServerConnectedListener(this));
         server.getScheduler().buildTask(this, new Runnable() {
 
             private int i = 0;
@@ -96,5 +144,13 @@ public class ProxyPlugin {
 
     public ProxyServer getServer() {
         return server;
+    }
+
+    public TomlParseResult getConfig() {
+        return config;
+    }
+
+    public ChannelIdentifier getBungeecordChannel() {
+        return bungeecordChannel;
     }
 }
