ee17c2663c2c34f3f028b4f5ae54f4f8a580e24b
Azodox_ <likemoi99@gmail.com>
2022-07-06 03:04:46 +0200

    Update Minecraft version.

--- src/main/java/fr/olten/proxy/listener/PingListener.java
+++ src/main/java/fr/olten/proxy/listener/PingListener.java
@@ -25,7 +25,7 @@ public class PingListener {
                         MiniMessage.miniMessage().deserialize("   <color:#FFFFFF>✳   ✳      <color:#FF0000>O<color:#BA0C0C>L<color:#ECFFEB>T<color:#2A850E>E<color:#27A300>N</color> <color:#FFFFFF><bold>✳</bold> <color:#FF0000>X<color:#BA0C0C>M<color:#ECFFEB>A<color:#2A850E>S</color><color:#FFFFFF>  <color:#FFFFFF><bold><italic>✳</italic></bold>  <color:dark_aqua>2<color:aqua>0<color:dark_aqua>2<color:aqua>2</color>  <color:#FFFFFF> ✳   ✳\n" +
                                 "   <color:#FFFFFF><bold><italic>✳</italic></bold>     <color:#FFFFFF><bold><italic>✳</italic></bold>       <color:gray>A<color:#ECFFEB>m<color:gray>u<color:#ECFFEB>s<color:gray>e<color:#ECFFEB>z <color:gray>v<color:#ECFFEB>o<color:gray>u<color:#ECFFEB>s b<color:#ECFFEB>i<color:gray>e<color:#ECFFEB>n<color:gray>!</color><color:#FFFFFF>  <color:#FFFFFF><bold>✳</bold>  <color:#FFFFFF><italic>✳</italic>     <color:#FFFFFF><bold>✳</bold>")
                 )
-                .version(new ServerPing.Version(758, "1.18.2"))
+                .version(new ServerPing.Version(759, "1.19"))
                 .onlinePlayers(proxyPlugin.getServer().getPlayerCount()).maximumPlayers(2022).build());
     }
 }
